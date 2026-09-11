8ecc31561831164f29ad269c22d7c7104d9518b6
DarkCat <meawdamxza@gmail.com>
2026-09-11 08:50:45 +0700

    Clear the unused features

--- frontend/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:frontend/calculator.dart';
-
-void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Calculator()));
-}
